71397995aed847a8af7193e9dddf5c954b1344d7
Renan Silva <renansupermanox@gmail.com>
2024-10-12 21:57:36 +0100

    update random tree

--- src/random-tree/sketch.js
+++ src/random-tree/sketch.js
@@ -1,34 +1,36 @@
-const depth = 5;
-const minLen = 20;
+const depth = 9;
+const minLen = 50;
 const maxLen = 100;
-const minAngle = -0.7;
-const maxAngle = 0.7;
+const minAngle = -0.65;
+const maxAngle = 0.65;
+const nNewBranches = 2;
+const arrayLength = getNumOfBranches(depth, nNewBranches);
 let lens = [];
 let angles = [];
 let i = 0;
 let j = 0;
+let hue = 120;
 
 function setup() {
+    console.log(arrayLength);
     createCanvas(windowWidth, windowHeight, WEBGL);
-
-    for (let i = 0; i < 10000; i++) {
-        lens.push(random(minLen, maxLen));
-    }
-
-    for (let i = 0; i < 10000; i++) {
-        angles.push(random(minAngle, maxAngle));
-    }
+    colorMode(HSL);
+    generateArrays();
 }
 
 function draw() {
+    camera(sin(frameCount * 0.02) * 1000, 0, cos(frameCount * 0.02) * 1000, 0, 0, 0, 0, 1, 0);
     normalMaterial();
     background(0);
-    orbitControl(3,3,3);
     scale(1,-1,1);
-    stroke(255, 100);
+    stroke(hue, 100, 50, 0.25);
+    strokeWeight(5);
 
-    translate(0,-150,0);
-    box(100,1,100)
+    translate(0,-300,0);
+    push();
+    noStroke();
+    box(100,5,100)
+    pop();
     drawBranch(depth);
 
     i = 0;
@@ -44,21 +46,56 @@ function drawBranch(depth){
         const len = lens[i];
         i++;
 
+        strokeWeight(map(depth, 5, 1, 20, 0));
         line(0, 0, 0, 0, len, 0);
         translate(0, len, 0);
 
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < nNewBranches/2; i++) {
             push();
             rotateX(angles[j]);
+            rotateY(angles[j]);
             drawBranch(depth - 1);
             pop();
             j++;
 
             push();
             rotateZ(angles[j]);
+            rotateY(angles[j]);
             drawBranch(depth - 1);
             pop();
             j++;
         }
     }
+}
+
+function generateArrays(){
+    lens = [];
+    angles = [];
+
+    for (let i = 0; i < arrayLength; i++) {
+        lens.push(random(minLen, maxLen));
+    }
+
+    for (let i = 0; i < arrayLength; i++) {
+        angles.push(random(minAngle, maxAngle));
+    }
+}
+
+function keyPressed(){
+    if(key === 'r'){
+        generateArrays();
+        hue = random(360);
+        i = 0;
+        j = 0;
+    }
+}
+
+function getNumOfBranches(depth, nNewBranchesByBranch){
+    let total = 0;
+
+    for (let i = 0; i < depth; i++) {
+        total += Math.pow(nNewBranchesByBranch, (i+1));
+    }
+
+    return total;
 }
